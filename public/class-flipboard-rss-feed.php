63603c105cc9ce8dcd2866e9c6f3bc10d119bfa2
simonmcwhinnie <simon.mcwhinnie@timeinc.com>
2015-01-09 12:06:35 +0000

    Remove var_dump from code

--- public/class-flipboard-rss-feed.php
+++ public/class-flipboard-rss-feed.php
@@ -479,7 +479,6 @@ class Flipboard_RSS_Feed {
                 'class'   => '',
         ), $attr, 'caption' );
 
-        var_dump($atts);
         $atts['width'] = (int) $atts['width'];
         if ( $atts['width'] < 1 || empty( $atts['caption'] ) )
                 return $content;
